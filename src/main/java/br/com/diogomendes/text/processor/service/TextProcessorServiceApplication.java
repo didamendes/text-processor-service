af5dc47ed084f3a57801377e39669112472c45f2
didamendes <didamendes@hotmail.com>
2025-04-18 19:06:32 -0300

    Começo do projeto

---
+++ src/main/java/br/com/diogomendes/text/processor/service/TextProcessorServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.diogomendes.text.processor.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextProcessorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextProcessorServiceApplication.class, args);
+	}
+
+}
